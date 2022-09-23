dabf2b67666cee170e8f0550f438ce06b03b4cc7
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-09-22 21:41:42 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) == 1):
+        print("{} argumnents.".format(len(sys.argv) - 1))
+    elif 0 < len(sys.argv) and len(sys.argv) > 1:
+        print("{} argumnents:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv) - 1):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
