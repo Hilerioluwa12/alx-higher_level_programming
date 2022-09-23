ba1648088ddaca0d0887a3505807875dc80cac76
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-09-22 21:59:56 -0700

    101-easy_print.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     import sys
     if (len(sys.argv) == 1):
         print("{} arguments.".format(len(sys.argv) - 1))
+
     elif 0 < len(sys.argv) and len(sys.argv) > 1:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(len(sys.argv) - 1):
