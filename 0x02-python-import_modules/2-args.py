513915043551ce6101956c5607af198e7506bc34
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-09-26 06:38:12 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv) == 1):
+    if (len(sys.argv) == 0):
         print("{} arguments.".format(len(sys.argv) - 1))
     elif (len(sys.argv) == 2):
         print("{} arguments:".format(len(sys.argv) - 1))
