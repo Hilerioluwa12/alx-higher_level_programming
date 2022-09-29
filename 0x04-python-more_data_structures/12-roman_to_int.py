702fd68b2af08e9428fe636a56bb4ed7df5a4665
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-09-28 21:46:04 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+def roamn_to_int(roman_string):
+    c = 0
+    result = 0
+    if not roman_string:
         return 0
-    d_2 = {"M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100,
-           "XC": 90, "L": 50, "XL": 40, "X": 10, "V": 5, "IV": 4, "I": 1}
-    response = 0
-    for i in range(len(roman_string)):
-        if i > 0 and d_2[roman_string[i]] > d_2[roman_string[i - 1]]:
-            response += d_2[roman_string[i]] - 2 * d_2[roman_string[i - 1]]
-        else:
-            response += d_2[roman_string[i]]
-        return response
+    if not isinstance(roman_string, str):
+        return 0
+    s = list(roman_string)
+    for l in range(len(s)):
+        arr = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        num = [1, 5, 10, 50, 100, 500, 1000]
+        for i in range(len(arr)):
+            if (s[l] == arr[i]):
+                s[l] = num[i]
+                break
+    if len(s) == 1:
+        return (s[0])
+    else:
+        for l in range(1, len(s)):
+            if s[l - 1] >= s[l]:
+                result = result + s[l - 1]
+            else:
+                result = result - s[l - 1]
+        result = result + s[l]
+        return result
