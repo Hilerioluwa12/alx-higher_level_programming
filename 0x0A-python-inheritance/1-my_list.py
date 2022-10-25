1b0aa028f5c26f1753b443030060a073b761b91c
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-24 18:21:47 -0700

    fix error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -17,13 +17,13 @@ class MyList(list):
         Arguments: None
         Returns: None
         """
+        super().__init__
 
     def print_sorted(self):
         """
         prints sorted list
-        
         Arguments: None
-        
+
         Returns: None
         """
 
