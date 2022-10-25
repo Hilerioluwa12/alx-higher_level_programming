fff902717af4614742d777283fbb5706c868a032
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-24 18:55:45 -0700

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,15 +5,11 @@ baseGeometry class
 
 
 class BaseGeometry:
-    def __init__(self):
-        """
-        empty class
-        """
-        pass
-
+    """"
+    BaseGeometry class
+    """
     def area(self):
         """
         expection raise with below error
         """
-
         raise Exception("area() is not implemented")
