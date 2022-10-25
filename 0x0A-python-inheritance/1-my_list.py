38e1affe6c195410699e5b5c9d0a19e18e0e72c4
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-24 18:03:00 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Class Mylist"""
+
+
+class Mylist(list):
+    """
+    Mylist class
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        initializes list
+
+        Arguments: None
+        Returns: None
+        """
+
+    def print_sorted(self):
+        """
+        prints sorted list
+        
+        Arguments: None
+        Returns:None
+        """
+
+        print(sorted(self))
