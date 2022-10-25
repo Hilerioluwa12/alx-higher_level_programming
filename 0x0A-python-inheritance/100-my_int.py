891a4bc129a7d048303b620fb8a505da31f56409
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-10-24 19:25:33 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,15 +6,9 @@ class MyInt(int):
     class Myint that inherits from int
     """
     def __eq__(self, other):
-    """ function that define equals to"""
-        if int(self) == int(other):
-            return False
-        else:
-            return True
+        """ function that define equals to"""
+        return int(self) != int(other)
 
     def __ne__(self, other):
-    """function that defines not equal to"""
-        if int(self) != int(other):
-            return False
-        else:
-            return True
+        """function that defines not equal to"""
+        return int(self) == int(other)
