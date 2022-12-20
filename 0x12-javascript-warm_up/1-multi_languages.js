7e6e0758a1420452c52d4ff6ffb9bcb2791c7223
Hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-12-20 02:24:08 +0000

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const first_line = "C is fun";
 const second_line = "Python is cool";
 const third_line = "JavaScript is amazing";
-
 console.log(first_line + '\n' + second_line + '\n' + third_line);
