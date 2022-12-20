a7e784d0fbaad71f5d938619ea830840503f3e01
Hilerioluwa12 <hardexilerioluwa@gmail.com>
2022-12-20 02:42:08 +0000

    fix 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* Prints 3 lines using an array of str and a loop */
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; arr[i]; i++) {
   console.log(arr[i]);
