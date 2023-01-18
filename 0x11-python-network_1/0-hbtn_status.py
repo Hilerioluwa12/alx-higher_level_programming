55c7078ed38c23ed22b3fde667cf7ebc0c896843
Hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-01-17 17:56:25 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-""" use urllib to get information from a web-site
-"""
-if __name__ == "__main__":
+
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+
+if __name__ == '__main__':
     import urllib.request
 
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    try:
-        with urllib.request.urlopen(req) as response:
-            result = response.read()
-            ty = type(result)
-            con = result
-            con_u = result.decode('utf-8')
-            print("Body response:")
-            print("\t- type: {}".format(ty))
-            print("\t- content:".format(con))
-            print("\t- utf8 content: {}".format(con_u))
-    except urllib.error.URLError as e:
-        print(e.reason)
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
