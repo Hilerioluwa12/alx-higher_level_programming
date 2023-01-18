df9fb423901c450b28edcb18b73b50cfb396ac10
Hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-01-17 18:11:02 -0800

    4-hbtn_status.py update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 if __name__ == '__main__':
     import requests
     r = requests.get("https://intranet.hbtn.io/status")
-    typ = type(r.text)
-    con = r.text
-    print("Body response:\n\t- type: {}\n\t- content: {}".format(typ, con))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
