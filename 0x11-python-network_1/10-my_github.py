af428025f00c48527d22bd4df221cee467625d5d
Hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-01-17 18:29:17 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-""" getting info of account from Github API using credentials
-"""
+"""Take GitHub credentials"""
+import requests
+import sys
+
 
 if __name__ == "__main__":
-    import sys
-    import requests
-    user = sys.argv[1]
-    pas = sys.argv[2]
-    API = "https://api.github.com/users"
-    API = API + "/{}".format(user)
-    r = requests.get(API, auth=requests.auth.HTTPBasicAuth(user, pas))
-    if r.status_code == 200:
-        print(r.json().get('id'))
-    else:
-        print("None")
+    url = 'https://api.github.com/user'
+    session = requests.Session()
+    response = session.get(url, auth=(sys.argv[1], sys.argv[2]))
+    r = response.json()
+    print(r.get('id'))
